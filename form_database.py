aa153a4fe05bfc748af2c1114a12b46392decd32
Grigory <barkev2009@bk.ru>
2021-12-12 01:44:24 +0300

    delete db from indexing

--- form_database.py
+++ form_database.py
@@ -57,35 +57,36 @@ def add_entries_by_year(year):
         conn.commit()
     artist_query = 'insert into main_app_artist (name, country_id) values ("{}", {})'
     song_query = 'insert into main_app_song (name, artist_id) values ("{}", {})'
-    entry_query = "insert into main_app_entry (contest_step_id, song_id, 'order', year_id, qualified, purity, " \
-                  "difficulty, originality, show, sympathy) values ({}, {}, {}, {}, 0, 0, 0, 0, 0, 0)"
+    entry_query = "insert into main_app_entry (contest_step_id, song_id, 'order', year_id, place, qualified, purity, " \
+                  "difficulty, originality, show, sympathy) values ({}, {}, {}, {}, {}, 0, 0, 0, 0, 0, 0)"
 
     for entry in entry_records:
         print(entry)
 
         full_artist_query = artist_query.format(
-            entry[-2],
-            countries.get(entry[-3])
+            entry[-3],
+            countries.get(entry[4])
         )
-        if entry[-2] not in get_all_artists().keys():
+        if entry[-3] not in get_all_artists().keys():
             print(full_artist_query)
             cursor.execute(full_artist_query)
             conn.commit()
 
         full_song_query = song_query.format(
-            entry[-1],
-            get_all_artists().get(entry[-2])
+            entry[-2],
+            get_all_artists().get(entry[-3])
         )
-        if entry[-1] not in get_all_songs().keys():
+        if entry[-2] not in get_all_songs().keys():
             print(full_song_query)
             cursor.execute(full_song_query)
             conn.commit()
 
         full_entry_query = entry_query.format(
             get_all_steps().get(entry[1]),
-            get_all_songs().get(entry[-1]),
+            get_all_songs().get(entry[-2]),
             entry[3],
-            get_all_years().get(entry[2])
+            get_all_years().get(entry[2]),
+            entry[-1]
         )
         print(full_entry_query)
         cursor.execute(full_entry_query)
@@ -103,7 +104,7 @@ def add_countries(countries_list: list):
 
 if __name__ == '__main__':
     # print(get_all_years())
-    add_entries_by_year(2013)
+    # add_entries_by_year(2009)
     countries = [
         "US, Iowa",
         "US, Alabama",
